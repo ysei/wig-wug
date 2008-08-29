45791af4dd6c147cb67c9da105fe418e0d696319
brabuhr <brabuhr@gmail.com>
2008-08-29 03:01:00 +0000

    fjc - refactor into smaller methods
    * primarly so subclasses can use weighting functions other than Math::log

--- lib/wig-wug/diggers/astar_digger/foostar_digger.rb
+++ lib/wig-wug/diggers/astar_digger/foostar_digger.rb
@@ -4,25 +4,40 @@ module WigWug
   module Diggers
 
     class FOOstarDigger < FAstarDigger
-      # stash a count of the number of times a position has been visited
       def pick_move
-        position = @board.position
-        board = @board.instance_variable_get("@board")
-        keys = board.keys
-        key = keys.find{|k| k == position}
-
-        v = key.instance_variable_get("@visited").to_i
-        key.instance_variable_set("@visited", v + 1)
-
+        stash_visit_count
         super
       end
 
     private
 
-      # cost is normal cost plus the log of the number of visits to that position
-      def set_cost k, v
-        t = k.instance_variable_get("@visited")
-        t ? super + Math::log(t).to_i : super
+      # stash a count of the number of times a position has been visited
+      # by attaching an instance variable to the board location key
+      def stash_visit_count
+        position = @board.position
+        board = @board.instance_variable_get("@board")
+        keys = board.keys
+        key = keys.find{|k| k == position}
+
+        visits = key.instance_variable_get("@visited").to_i
+        key.instance_variable_set("@visited", visits + 1)
+      end
+
+      # fetch the count of the number of times a position has been visited
+      # from the inserted instance variable
+      def fetch_visit_count key
+        key.instance_variable_get("@visited")
+      end
+
+      # cost is normal cost plus the function of the number of visits to that position
+      def set_cost key, _
+        visits = fetch_visit_count(key)
+        visits ? super + visit_weight(visits).to_i : super
+      end
+
+      # default visit weight function is to take the log
+      def visit_weight visits
+        Math::log(visits)
       end
     end
 
